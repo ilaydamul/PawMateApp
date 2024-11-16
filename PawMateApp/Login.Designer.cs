e7972e5bf954ca4183defe236a4392d3a42c2f32
sila1313 <sila.bsrn123@gmail.com>
2024-11-16 17:58:39 +0300

    Login kısmı için bağlantı yapıldı.

--- PawMateApp/Login.Designer.cs
+++ PawMateApp/Login.Designer.cs
@@ -46,28 +46,32 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(14, 146);
+            this.label2.Location = new System.Drawing.Point(19, 180);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 14);
+            this.label2.Size = new System.Drawing.Size(40, 16);
             this.label2.TabIndex = 6;
             this.label2.Text = "Şifre";
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(227, 168);
+            this.checkBox1.Location = new System.Drawing.Point(303, 207);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.Size = new System.Drawing.Size(18, 17);
             this.checkBox1.TabIndex = 5;
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(15, 99);
+            this.label1.Location = new System.Drawing.Point(20, 122);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 14);
+            this.label1.Size = new System.Drawing.Size(92, 16);
             this.label1.TabIndex = 6;
             this.label1.Text = "Kullanıcı Adı";
             // 
@@ -76,9 +80,10 @@
             this.lbl_goSignUp.AutoSize = true;
             this.lbl_goSignUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.lbl_goSignUp.LinkColor = System.Drawing.Color.Black;
-            this.lbl_goSignUp.Location = new System.Drawing.Point(14, 192);
+            this.lbl_goSignUp.Location = new System.Drawing.Point(19, 236);
+            this.lbl_goSignUp.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_goSignUp.Name = "lbl_goSignUp";
-            this.lbl_goSignUp.Size = new System.Drawing.Size(156, 13);
+            this.lbl_goSignUp.Size = new System.Drawing.Size(193, 16);
             this.lbl_goSignUp.TabIndex = 4;
             this.lbl_goSignUp.TabStop = true;
             this.lbl_goSignUp.Text = "Henüz hesabın mı yok? Kayıt Ol";
@@ -89,28 +94,32 @@
             this.btn_login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(33)))), ((int)(((byte)(230)))));
             this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_login.ForeColor = System.Drawing.Color.White;
-            this.btn_login.Location = new System.Drawing.Point(17, 218);
+            this.btn_login.Location = new System.Drawing.Point(23, 268);
+            this.btn_login.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btn_login.Name = "btn_login";
-            this.btn_login.Size = new System.Drawing.Size(226, 37);
+            this.btn_login.Size = new System.Drawing.Size(301, 46);
             this.btn_login.TabIndex = 2;
             this.btn_login.Text = "Giriş Yap";
             this.btn_login.UseVisualStyleBackColor = false;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
             // 
             // txt_password
             // 
             this.txt_password.BackColor = System.Drawing.Color.White;
-            this.txt_password.Location = new System.Drawing.Point(17, 164);
+            this.txt_password.Location = new System.Drawing.Point(23, 202);
+            this.txt_password.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_password.Name = "txt_password";
             this.txt_password.PasswordChar = '*';
-            this.txt_password.Size = new System.Drawing.Size(226, 20);
+            this.txt_password.Size = new System.Drawing.Size(300, 22);
             this.txt_password.TabIndex = 1;
             // 
             // logoBox
             // 
             this.logoBox.Image = ((System.Drawing.Image)(resources.GetObject("logoBox.Image")));
-            this.logoBox.Location = new System.Drawing.Point(17, 12);
+            this.logoBox.Location = new System.Drawing.Point(23, 15);
+            this.logoBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.logoBox.Name = "logoBox";
-            this.logoBox.Size = new System.Drawing.Size(226, 72);
+            this.logoBox.Size = new System.Drawing.Size(301, 89);
             this.logoBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.logoBox.TabIndex = 1;
             this.logoBox.TabStop = false;
@@ -118,9 +127,10 @@
             // txt_username
             // 
             this.txt_username.BackColor = System.Drawing.Color.White;
-            this.txt_username.Location = new System.Drawing.Point(17, 117);
+            this.txt_username.Location = new System.Drawing.Point(23, 144);
+            this.txt_username.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txt_username.Name = "txt_username";
-            this.txt_username.Size = new System.Drawing.Size(226, 20);
+            this.txt_username.Size = new System.Drawing.Size(300, 22);
             this.txt_username.TabIndex = 0;
             // 
             // panel1
@@ -134,20 +144,23 @@
             this.panel1.Controls.Add(this.txt_username);
             this.panel1.Controls.Add(this.lbl_goSignUp);
             this.panel1.Controls.Add(this.txt_password);
-            this.panel1.Location = new System.Drawing.Point(271, 88);
+            this.panel1.Location = new System.Drawing.Point(361, 108);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
             this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.panel1.Size = new System.Drawing.Size(258, 273);
+            this.panel1.Size = new System.Drawing.Size(344, 336);
             this.panel1.TabIndex = 2;
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Giriş Yap";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Login_Paint);
             ((System.ComponentModel.ISupportInitialize)(this.logoBox)).EndInit();
--- PawMateApp/Login.cs
+++ PawMateApp/Login.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,6 +19,13 @@ namespace PawMateApp
             InitializeComponent();
         }
 
+        public static class Globals
+        {
+            public static int CurrentUserID { get; set; } 
+        }
+
+        NpgsqlConnection baglan = new NpgsqlConnection("server=localHost; port=5432; Database=pawmatedb; user ID=postgres; password=sila123");
+                                                                                                                           //şifreyi kendi veritabanı şifrenize göre değiştirin.
         private void Login_Paint(object sender, PaintEventArgs e)
         {
             Color leftColor = ColorTranslator.FromHtml("#B5B3F1");
@@ -28,5 +36,49 @@ namespace PawMateApp
                 e.Graphics.FillRectangle(lgb, this.ClientRectangle);
             }
         }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            if (txt_username.Text == "" || txt_password.Text == "")
+            {
+                MessageBox.Show("Lütfen boş alanları doldurunuz.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                baglan.Open();
+                NpgsqlCommand cmd = new NpgsqlCommand("Select * from users where username=@P1 AND password=@P2", baglan);
+                cmd.Parameters.AddWithValue("@P1", txt_username.Text);
+                cmd.Parameters.AddWithValue("@P2", txt_password.Text);
+                NpgsqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    MessageBox.Show("Giriş Başarıyla Yapıldı!" );
+                    int user_id = Convert.ToInt32(dr["user_id"]);
+                    Globals.CurrentUserID = user_id;
+                }
+                else
+                {
+                    MessageBox.Show("Kullanıcı adı ya da şifre yanlış.", "Hatalı Giriş!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                baglan.Close();
+            }
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                // Şifreyi görünür yap
+                txt_password.PasswordChar = '\0';
+            }
+            else
+            {
+                // Şifreyi gizle
+                txt_password.PasswordChar = '*';
+            }
+        }
     }
 }
+
