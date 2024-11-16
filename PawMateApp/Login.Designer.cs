a2becbad006a7dcd4733ebd13e36b79dda6a813e
ilaydamul <Ilayda2003.1905@hotmail.com>
2024-11-16 15:51:35 +0300

    Giriş Yap sayfasının başlangıcı

---
+++ PawMateApp/Login.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace PawMateApp
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_goSignUp = new System.Windows.Forms.LinkLabel();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.logoBox = new System.Windows.Forms.PictureBox();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.logoBox)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 14);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Şifre";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(227, 168);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 14);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Kullanıcı Adı";
+            // 
+            // lbl_goSignUp
+            // 
+            this.lbl_goSignUp.AutoSize = true;
+            this.lbl_goSignUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_goSignUp.LinkColor = System.Drawing.Color.Black;
+            this.lbl_goSignUp.Location = new System.Drawing.Point(14, 192);
+            this.lbl_goSignUp.Name = "lbl_goSignUp";
+            this.lbl_goSignUp.Size = new System.Drawing.Size(156, 13);
+            this.lbl_goSignUp.TabIndex = 4;
+            this.lbl_goSignUp.TabStop = true;
+            this.lbl_goSignUp.Text = "Henüz hesabın mı yok? Kayıt Ol";
+            this.lbl_goSignUp.VisitedLinkColor = System.Drawing.Color.Black;
+            // 
+            // btn_login
+            // 
+            this.btn_login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(33)))), ((int)(((byte)(230)))));
+            this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_login.ForeColor = System.Drawing.Color.White;
+            this.btn_login.Location = new System.Drawing.Point(17, 218);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(226, 37);
+            this.btn_login.TabIndex = 2;
+            this.btn_login.Text = "Giriş Yap";
+            this.btn_login.UseVisualStyleBackColor = false;
+            // 
+            // txt_password
+            // 
+            this.txt_password.BackColor = System.Drawing.Color.White;
+            this.txt_password.Location = new System.Drawing.Point(17, 164);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.PasswordChar = '*';
+            this.txt_password.Size = new System.Drawing.Size(226, 20);
+            this.txt_password.TabIndex = 1;
+            // 
+            // logoBox
+            // 
+            this.logoBox.Image = ((System.Drawing.Image)(resources.GetObject("logoBox.Image")));
+            this.logoBox.Location = new System.Drawing.Point(17, 12);
+            this.logoBox.Name = "logoBox";
+            this.logoBox.Size = new System.Drawing.Size(226, 72);
+            this.logoBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.logoBox.TabIndex = 1;
+            this.logoBox.TabStop = false;
+            // 
+            // txt_username
+            // 
+            this.txt_username.BackColor = System.Drawing.Color.White;
+            this.txt_username.Location = new System.Drawing.Point(17, 117);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(226, 20);
+            this.txt_username.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.logoBox);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.btn_login);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txt_username);
+            this.panel1.Controls.Add(this.lbl_goSignUp);
+            this.panel1.Controls.Add(this.txt_password);
+            this.panel1.Location = new System.Drawing.Point(271, 88);
+            this.panel1.Name = "panel1";
+            this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.panel1.Size = new System.Drawing.Size(258, 273);
+            this.panel1.TabIndex = 2;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.Text = "Giriş Yap";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Login_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.logoBox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel lbl_goSignUp;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.PictureBox logoBox;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ PawMateApp/Login.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PawMateApp
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Paint(object sender, PaintEventArgs e)
+        {
+            Color leftColor = ColorTranslator.FromHtml("#B5B3F1");
+            Color rightColor = ColorTranslator.FromHtml("#2A21DC");
+
+            using (LinearGradientBrush lgb = new LinearGradientBrush(this.ClientRectangle, leftColor, rightColor, 0F))
+            {
+                e.Graphics.FillRectangle(lgb, this.ClientRectangle);
+            }
+        }
+    }
+}
